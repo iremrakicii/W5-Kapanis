df1ad4fcd846df491d9f6643da95a4f1cb7ea413
İrem Rakıcı <iremrakici53@hotmail.com>
2024-10-30 20:54:09 +0300

    Proje dosyası ekle.

---
+++ W5-Kapanis/Araba.cs
@@ -0,0 +1,89 @@
+﻿namespace W5_Kapanis
+{
+    public class Araba
+    {
+        private DateTime _uretımTarıhı;
+        private string _seriNumarasi;
+        private string _marka;
+        private string _model;
+        private string _renk;
+        private int _kapiSayisi;
+
+        public DateTime uretımTarıhı
+        {
+            get
+            {
+                return _uretımTarıhı;
+            }
+            set
+            {
+                _uretımTarıhı = value;
+            }
+        }
+        public string seriNumarasi
+        {
+            get
+            {
+                return _seriNumarasi;
+            }
+            set
+            {
+                _seriNumarasi = value;
+            }
+        }
+        public string marka
+        {
+            get
+            {
+                return _marka;
+            }
+            set
+            {
+                _marka = value;
+            }
+        }
+        public string model
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                _model = value;
+            }
+        }
+        public string renk
+        {
+            get
+            {
+                return _renk;
+
+            }
+            set
+            {
+                _renk = value;
+            }
+        }
+        public int kapiSayisi
+        {
+            get
+            {
+                return _kapiSayisi;
+            }
+            set
+            {
+                _kapiSayisi = value;
+            }
+
+        }
+        public void BilgileriYazdir()
+        {
+            Console.WriteLine($"Modeli: {_model}, Markası: {_marka}, Rengi: {_renk}, Kapı Sayısı: {_kapiSayisi}, Seri Numarası: {_seriNumarasi}, Üretim Tarihi: {_uretımTarıhı}");
+        }
+
+
+
+
+    }
+}
---
+++ W5-Kapanis/Program.cs
@@ -0,0 +1,80 @@
+﻿using W5_Kapanis;
+
+class Program
+{
+    static void Main()
+    {
+        bool Value = false;
+        while (!Value)
+        {
+        baslangic1:
+            try
+            {
+                Console.WriteLine("Merhaba Araba üretmek ister misiniz? Üretmek isterseniz 'e' harfini istemiyorsanız 'h' harfini tuşlayın.");
+                string cevap = Console.ReadLine().ToLower();
+
+
+                if (cevap == "h")
+                {
+                    Console.WriteLine("Program sonlandırılıyor...");
+                    break;
+                }
+
+                else if (cevap == "e")
+                {
+                    List<Araba> Arabalar = new List<Araba>();
+                    Araba araba = new Araba();
+                    Console.WriteLine("İstediğiniz arabanın aşağıdaki bilgilerini girer misiniz?");
+                    Console.Write("Modeli:"); araba.model = Console.ReadLine();
+                    Console.Write("Markası:"); araba.marka = Console.ReadLine();
+                    Console.Write("Rengi:"); araba.renk = Console.ReadLine();
+
+                baslangic2:
+                    Console.Write("Kapı sayısı:");
+                    try
+                    {
+                        araba.kapiSayisi = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Hatalı giriş yaptınız. Lütfen tekrar giriş yapın.");
+                        goto baslangic2;
+                    }
+
+                    Console.Write("Seri Numarası:"); araba.seriNumarasi = Console.ReadLine();
+                    araba.uretımTarıhı = DateTime.Now;
+                    Console.Write($"Üretim Tarihi: {araba.uretımTarıhı}");
+
+                    Arabalar.Add(araba);
+                    Console.WriteLine("Araba başarıyla üretildi.");
+                    Console.WriteLine("Yeni bir araba üretmek ister misiniz? (e/h)");
+                    string cevap2 = Console.ReadLine().ToLower();
+                    if (cevap2 == "h")
+                    {
+                        Console.WriteLine("\nAraba Listesi:");
+                        foreach (var item in Arabalar)
+                        {
+                            item.BilgileriYazdir();
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tekrardan araba üretmek için sizi anasayfaya yönlendiriyorum!");
+                        goto baslangic1;
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Geçersiz formatta işlem yaptınız.");
+            }
+            finally
+            {
+
+            }
+        }
+
+
+    }
+}
